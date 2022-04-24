3a156f53f085d9c57c137f468936e6e1451b9eaa
Juliano S. Silva <julianosoaresdasilva@outlook.com>
2022-04-23 21:59:24 -0300

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 
-console.log("Hello World!");
+import express from "express";
+import { loadRoutes } from "./main/routes";
+
+let app = express();
+let port = 8000;
+
+app.use(loadRoutes());
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
---
+++ src/main/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+export function loadRoutes() {
+    
+    let router = Router();
+
+    router.get("/", (req, res) => {
+        res.send("Hello World!")
+    })
+
+    return router;
+}

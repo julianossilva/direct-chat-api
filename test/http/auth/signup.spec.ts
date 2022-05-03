41139885c982a496eb38c01df9cd19ba3f66cf69
Juliano S. da Silva <julianosoaresdasilva@outlook.com>
2022-05-03 07:49:14 -0300

    clean database before of each test

--- test/http/auth/signup.spec.ts
+++ test/http/auth/signup.spec.ts
@@ -1,16 +1,28 @@
 import dotenv from "dotenv";
+import { Pool } from "pg";
 import supertest from "supertest";
 import { CleanDatabase } from "../../../src/infra/database/command/clean-database";
 import { App } from "../../../src/main/app";
 
 dotenv.config();
 
+beforeEach(async () => {
+    let pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+    });
+    await new CleanDatabase(pool).run();
+    await pool.end()
+});
+
 test("POST /signup", async () => {
     dotenv.config();
 
     let app = new App();
 
-    await new CleanDatabase(app.appContext.pool).run()
     await supertest(app.httpServer)
         .post("/signup")
         .send({
@@ -18,7 +30,7 @@ test("POST /signup", async () => {
             password: "12345678",
             name: "Juliano",
         })
-        .expect(201)
-        
+        .expect(201);
+
     await app.appContext.shutdown();
 });
